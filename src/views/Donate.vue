7d9e54de8fcdd3d33657970962bd58bab1b2425f
JustQiyi <justqiyi@qq.com>
2025-07-31 02:55:49 +0800

    feat: add afdian donate link

--- src/views/Donate.vue
+++ src/views/Donate.vue
@@ -17,13 +17,20 @@
 				<h1 v-view.once="flowUp" class="primary-text">如何捐助</h1>
 				<p v-view.once="flowUp">免责声明：<strong>未满 18 岁的未成年人请在家长同意后进行捐助，竹像素 从未教唆未成年人「为游戏充值」。捐助即代表你已经阅读并知晓该声明。</strong></p>
 				<p v-view.once="flowUp">
-					此处提供支付宝和微信支付的二维码，如需其它支付方式（不支持 QQ 支付）可联系服主。若要捐助，扫描二维码即可，付款时<strong>请备注你的 Minecraft 用户名</strong>，以便我们写入鸣谢名单（低于
+					此处提供爱发电链接，如需其它支付方式（如直接支付，不支持 QQ 支付）可联系服主。若要捐助，扫描二维码即可，付款时<strong>请备注你的 Minecraft 用户名</strong>，以便我们写入鸣谢名单（低于
 					<span class="money">￥5</span>
 					的款项恕不写入）。支持
 					<strong>5 天无理由全额退款</strong>，超过 5 天若仍因某些原因仍需退款可联系服主。
 				</p>
 				<div v-view.once="flowUp" class="pay">
-					暂时不接受捐助
+					<a class="donate-card afdian" href="https://afdian.tv/a/zhuxs" target="_blank">
+						<mdicon name="arrow-right" />
+						<img src="https://static.afdiancdn.com/static/img/logo/logo.png" />
+						<div class="text">
+							<h1>爱发电</h1>
+							<h2>点击跳转至官方爱发电</h2>
+						</div>
+					</a>
 				</div>
 				<h1 v-view.once="flowUp" class="primary-text">感谢捐助</h1>
 				<p v-view.once="flowUp">感谢这些人的无偿捐助，帮助了 竹像素 发展和维持运行。排名使用洗牌算法随机排列，刷新页面可重排。</p>
@@ -94,6 +101,15 @@ export default Vue.extend({
 		@media (max-width: 800px) {
 			height: 200px;
 		}
+	}
+	.donate-card {
+		@media (min-width: 1000px) {
+			width: 50%;
+		}
+		@media (max-width: 1000px) {
+			margin: 8px 0;
+		}
+		.link-card;
 	}
 }
 </style>
