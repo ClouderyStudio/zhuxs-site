607a218de03c8324925c9aa354b85c04284b34a7
TulipQiyi <justqiyi@outlook.com>
2026-09-06 15:24:49 +0800

    refactor: 移除失效 SEO mixin、SW 注册与遗留 shims 文件

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import "@/styles/global.less";
 import "@/styles/mc.less";
 import "@/styles/nprogress.less";
+import { ASSET_BASE } from "@/config";
 import * as mdijs from "@mdi/js";
 import mdiVue from "mdi-vue/v3";
 import VueLazyload from "vue3-lazyload";
@@ -29,15 +29,15 @@ app.use(mdiVue, {
   icons: mdijs,
 });
 app.use(VueLazyload, {
-  loading: "https://a.cldery.com/d/oss/mcweb/assets/loading.gif",
-  error: "https://a.cldery.com/d/oss/mcweb/assets/error.png",
+  loading: `${ASSET_BASE}/loading.gif`,
+  error: `${ASSET_BASE}/error.png`,
 });
 app.use(head);
 
 app.config.globalProperties.$open = (url: string) => {
   window.open(url);
 };
-app.config.globalProperties.load = false;
+
 router.beforeEach((to, from, next) => {
   NProgress.start();
   next();
@@ -48,57 +48,9 @@ router.afterEach((to, from) => {
     document.getElementsByTagName("html")[0].scrollTo(0, 0);
   }
   NProgress.done();
-  app.config.globalProperties.load = true;
 });
 
 app.use(router);
 app.use(store);
-
-app.mixin({
-  metaInfo: {
-    meta: [
-      { property: "og:title", content: "竹像素 - BambooPixel" },
-      { property: "og:site_name", content: "竹像素" },
-      { property: "og:type", content: "website" },
-      { property: "og:url", content: "https://zhuxs.cn" },
-      {
-        property: "og:image",
-        content: "@/titanium.png",
-      },
-      {
-        property: "og:description",
-        content:
-          "这里是竹像素服务器官方网站，你可以在这里获取到关于竹像素的介绍以及服务器的运行信息。",
-      },
-      { itemprop: "name", content: "竹像素 | Bamboo Pixel" },
-      {
-        itemprop: "description",
-        content:
-          "这里是竹像素服务器官方网站，你可以在这里获取到关于竹像素的介绍以及服务器的运行信息。",
-      },
-      {
-        itemprop: "image",
-        content: "@/titanium.png",
-      },
-      { httpEquiv: "Content-Type", content: "text/html; charset=utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      {
-        name: "description",
-        content:
-          "这里是竹像素服务器官方网站，你可以在这里获取到关于竹像素的介绍以及服务器的运行信息。",
-      },
-      {
-        name: "theme-color",
-        content: "#54F47F",
-      },
-    ],
-    link: [
-      {
-        rel: "canonical",
-        href: "https://zhuxs.cn",
-      },
-    ],
-  },
-});
 
 app.mount("#app");
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createMemoryHistory,createWebHistory, createRouter } from 'vue-router';
+import { createWebHistory, createRouter } from 'vue-router';
 
 const routes = [
 	{
